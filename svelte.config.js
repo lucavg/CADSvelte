7e3db57245184009bac1319f23f1e7927a75a7a5
Luca Van Genechten <71765347+lucavg@users.noreply.github.com>
2023-07-20 20:39:37 +0200

    added starter template

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,59 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+/* jshint esversion: 11 */
+
+// Imports
+import preprocess from 'svelte-preprocess';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// Adapters
+import vercelAdapter from '@sveltejs/adapter-vercel';
+
+// Custom require function as replacement for the require from the commonJS in ES Module
+
+// Custom __dirname as replacement for the __dirname from the commonJS in ES Module
+const __dirname = dirname(fileURLToPath(import.meta.url)); // jshint ignore:line
+
+const options = JSON.stringify(process.env.OPTIONS || '{}');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		preprocess({
+			postcss: true,
+			preserve: ['ld+json', 'module'],
+			typescript: true,
+		}),
+	],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: vercelAdapter(options),
+		target: '#starter',
+		ssr: true,
+		prerender: {
+			crawl: true,
+			enabled: true,
+			onError: 'fail',
+			pages: ['*'],
+		},
+		vite: () => ({
+			resolve: {
+				alias: {
+					$stores: resolve(__dirname, './src/stores'),
+					$components: resolve(__dirname, './src/lib/shared/components'),
+					$ui: resolve(__dirname, './src/lib/shared/ui'),
+					$layouts: resolve(__dirname, './src/lib/layouts'),
+					$shared: resolve(__dirname, './src/lib/shared'),
+					$models: resolve(__dirname, './src/lib/models'),
+					$data: resolve(__dirname, './src/lib/data'),
+					$core: resolve(__dirname, './src/lib/core'),
+					$utils: resolve(__dirname, './src/lib/utils'),
+					$environment: resolve(__dirname, './src/environments'),
+				},
+			},
+			envPrefix: ['VITE_', 'SVELTEKIT_STARTER_'],
+			plugins: [],
+		}),
+	},
 };
 
 export default config;
