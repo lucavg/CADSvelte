7385ccca2daf53bbf13550ab706e5db41ff27bf5
Luca Van Genechten <71765347+lucavg@users.noreply.github.com>
2023-12-28 16:27:06 +0100

    Update +layout.svelte
    Added loading spinner while navigating

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
+	import { navigating } from '$app/stores';
 </script>
 
 <div class="flex flex-col min-h-screen text-black bg-gray-100 h-4/5">
@@ -9,9 +10,15 @@
 		<Navbar />
 	</div>
 
-	<main class="w-10/12 md:w-8/12 flex-grow container mx-auto my-5">
-		<slot />
-	</main>
+	{#if $navigating}
+		<div class="flex items-center justify-center min-h-screen">
+			<span class="loading loading-spinner text-primary loading-lg" />
+		</div>
+	{:else}
+		<main class="w-10/12 md:w-8/12 flex-grow container mx-auto my-5">
+			<slot />
+		</main>
+	{/if}
 </div>
 
 <div class="w-full bg-gray-100">
